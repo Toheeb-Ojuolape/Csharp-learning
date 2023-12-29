f51604e0d2206f29e16397fb2adca4480770327e
Toheeb Ojuolape <45619240+Toheeb-Ojuolape@users.noreply.github.com>
2023-12-30 00:44:02 +0100

    first commit: if and foreach

---
+++ CsharpProjects/TestProject/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+//create new C# project dotnet new console -o ./CsharpProjects/TestProject
+// Random dice = new Random();
+// int roll1 = dice.Next();
+// int roll2 = dice.Next(101);
+// int roll3 = dice.Next(50,101);
+
+// Console.WriteLine($"First role: {roll1}");
+// Console.WriteLine($"Second role: {roll2}");
+// Console.WriteLine($"Third role: {roll3}");
+using System;
+
+int firstValue = 500;
+int secondValue = 600;
+int largerValue = Math.Max(firstValue, secondValue);
+
+Console.WriteLine(largerValue);
+
+// maximum value of an int is 2,147,483,647
+
---
+++ Programs/Program.cs
@@ -0,0 +1,11 @@
+﻿string[] frauds = { "B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179" };
+string[] fraudulent_users = { };
+foreach (string fraud in frauds)
+{
+    if (fraud.StartsWith('B'))
+    {
+        Console.WriteLine(fraud);
+    }
+}
+
+Console.WriteLine(fraudulent_users.ToString());
